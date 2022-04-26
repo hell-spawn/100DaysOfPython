0835355200bb7c9c5345c8cac170cb2a6702d4da
hell.spawn <mauricio.hell.spawn@gmail.com>
2022-04-25 22:13:29 -0500

    Day - 001

---
+++ day-001/README.md
@@ -0,0 +1,21 @@
+# Day 001
+
+## Function print
+```python
+    print()
+```
+- new Line: '\n'
+- String concatenation: '+'
+- Coment line: #
+
+
+## Function input
+
+```python
+    input()
+```
+## Function len
+get length of string
+```python
+    len()
+```
---
+++ day-001/main.py
@@ -0,0 +1,27 @@
+print("Hello world!")
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+print('String Concatenation is done with the "+" sign.')
+print("New lines can be created \nwith a backslash and n.")
+
+#Input and Len
+print(len(input('What is your name?')))
+
+# variable 
+
+name = input("What is your name... again")
+print(name)
+length = len(name)
+print(length)
+
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator!!!!")
+
+#2. Ask the user for the city that they grew up in.
+city = input("Which city did grow up in?")
+#3. Ask the user for the name of a pet.
+pet = input("what is the name of de pet?")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be: " + city + " " + pet)
+
