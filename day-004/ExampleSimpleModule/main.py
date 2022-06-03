3c03c14dda5855d5b331ed988fe545bc881bd600
hell.spawn <mauricio.hell.spawn@gmail.com>
2022-06-02 22:04:43 -0500

    Day 004

---
+++ day-004/ExampleSimpleModule/main.py
@@ -0,0 +1,14 @@
+import random
+import my_module
+
+#User my custom module
+print(my_module.pi)
+
+# Random Integer
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+# Simple Random Float
+random_float = random.random();
+print(random_float)
+
---
+++ day-004/README.md
@@ -0,0 +1,25 @@
+# Modules
+
+user key word import 
+
+```python
+
+import random
+
+print(random.random())
+
+```
+
+
+# List
+
+```python
+
+fruits = ["Apple", "Orange", "Banana"]
+vegetables = ["Tomato", "Onion"]
+
+food = [fruits, vegetables]
+
+```
+
+
